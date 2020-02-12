67c48242f60c9df84e4c09c5b94fb3402b93aef5
e <837601718@qq.com>
2020-02-12 14:50:35 +0800

    zzzz

---
+++ config/sysConfig.js
@@ -0,0 +1,11 @@
+var sysConfig = {
+    xcApiUrlPre: '/api',
+    // xcApiUrl: 'http://api.xuecheng.com/',
+    xcApiUrl: 'http://localhost:11000/cms',
+    imgUrl:'http://img.xuecheng.com/',
+    videoUrl:'http://video.xuecheng.com/',
+    openAuthenticate:true,
+    openAuthorize:true
+}
+
+module.exports = sysConfig
